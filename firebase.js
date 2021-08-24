6aa8d7ecf3c9afef4facc3f58f2a0fed1eee3ebc
Obeboinnocent <obeboinnocent25@gmail.com>
2021-08-24 09:04:47 +0300

    reinitialized files

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyABdaCja4PhFgoR6mQa_LI_v0yla8DoDzo",
+    authDomain: "clone-2-11f63.firebaseapp.com",
+    projectId: "clone-2-11f63",
+    storageBucket: "clone-2-11f63.appspot.com",
+    messagingSenderId: "636451373010",
+    appId: "1:636451373010:web:a9487c25267ce7af1b120b",
+    measurementId: "G-YK6333K92B"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
